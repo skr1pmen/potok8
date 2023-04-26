b9abe5a200304d8bbe49d303948e17177a9baf0c
SchollSaaS <v.tsarukyan@doslab.ru>
2023-04-26 20:43:35 +0400

    les8

---
+++ les8/index.php
@@ -0,0 +1,10 @@
+<?php
+//function user($name, $address, $age){
+//    echo "<h2>Твоё имя: $name<br>
+//            Твой адрес: $address<br>
+//            Твой вораст: $age</h2>";
+//}
+//user("Артём", "Красная Набережная 12", 34);
+
+$num = 445;
+var_dump(substr($num, 0, 9));
